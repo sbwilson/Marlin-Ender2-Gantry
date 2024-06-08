1a06d47fb3a938d87f4a96923dce69dca4feb5c1
Simon Wilson <simon@xeosdd.net>
2024-06-08 18:29:46 +1000

    fix: adjusts the bed size to probe properly

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -116,7 +116,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "3D Printer"
+#define CUSTOM_MACHINE_NAME "CamPlatform"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1821,16 +1821,16 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 165
-#define Y_BED_SIZE 165
+#define X_BED_SIZE 150
+#define Y_BED_SIZE 150
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -25
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 165
+#define X_MAX_POS X_BED_SIZE + 25
+#define Y_MAX_POS Y_BED_SIZE + 0
+#define Z_MAX_POS 200
 #define I_MIN_POS 0
 #define I_MAX_POS 90
 #define J_MIN_POS -360
